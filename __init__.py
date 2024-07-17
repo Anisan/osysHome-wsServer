b7a928bb134ba5424f68e959352e8c18749fc333
Eraser <eraser1981@gmail.com>
2024-07-17 08:25:14 +0300

    fixed

--- __init__.py
+++ __init__.py
@@ -4,23 +4,22 @@ import datetime
 from flask_socketio import SocketIO
 from flask import render_template, request
 from flask_login import current_user
-from app.extensions import cache
 from app.core.main.BasePlugin import BasePlugin
-from app.core.lib.object import getObject,callMethod,setProperty
+from app.core.lib.object import getObject, callMethod, setProperty
 
 
 class wsServer(BasePlugin):
-    """ Websocket Server module """
+    """Websocket Server module"""
 
-    def __init__(self,app):
-        super().__init__(app,__name__)
+    def __init__(self, app):
+        super().__init__(app, __name__)
         self.title = "Websocket"
         self.description = """Websocket server (SocketIO)"""
         self.category = "System"
-        self.actions=['say','proxy']
-        #Dictionary connected clients
+        self.actions = ["say", "proxy"]
+        # Dictionary connected clients
         self.connected_clients = {}
-        #ws
+        # ws
         self.socketio = SocketIO(logger=False, engineio_logger=False)
         self.register_websocket(app)
 
@@ -35,8 +34,8 @@ class wsServer(BasePlugin):
         def wsTest():
             return render_template("ws_test.html")
 
-    def register_websocket(self,app):
-        """ Register websocket in app"""
+    def register_websocket(self, app):
+        """Register websocket in app"""
         self.socketio.init_app(app)
 
         @self.socketio.on("connect")
@@ -44,23 +43,27 @@ class wsServer(BasePlugin):
             try:
                 if not current_user.is_authenticated:
                     return False
-                self.logger.debug("Client %s(%s) connected", request.remote_addr, request.sid)
-                #append connected clients
+                self.logger.debug(
+                    "Client %s(%s) connected", request.remote_addr, request.sid
+                )
+                # append connected clients
                 self.connected_clients[request.sid] = {
-                        "username": current_user.username,
-                        "ip": request.remote_addr,
-                        "connected": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-                        "transport": self.socketio.server.transport(request.sid),
-                        "subsProperties": [],
-                        "subsObjects": [],
-                    }
+                    "username": current_user.username,
+                    "ip": request.remote_addr,
+                    "connected": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                    "transport": self.socketio.server.transport(request.sid),
+                    "subsProperties": [],
+                    "subsObjects": [],
+                }
             except Exception as ex:
                 self.logger.exception(ex, exc_info=True)
 
         @self.socketio.on("disconnect")
         def handleDisconnect():
             try:
-                self.logger.debug("Client %s(%s) disconnected", request.remote_addr, request.sid)
+                self.logger.debug(
+                    "Client %s(%s) disconnected", request.remote_addr, request.sid
+                )
                 self.connected_clients.pop(request.sid, None)
             except Exception as ex:
                 self.logger.exception(ex, exc_info=True)
@@ -71,11 +74,12 @@ class wsServer(BasePlugin):
 
         @self.socketio.on("restart_system")
         def handleRestart():
-            self.logger.info("Command restart system from %s",current_user.username)
+            self.logger.info("Command restart system from %s", current_user.username)
             if current_user.role == "admin":
                 from app.admin.tools import restart_system
-                restart_system()    
-        
+
+                restart_system()
+
         @self.socketio.on("message")
         def handleMessage(message):
             try:
@@ -89,7 +93,7 @@ class wsServer(BasePlugin):
             try:
                 self.logger.debug("Get clients")
                 sid = request.sid
-                for _,client in self.connected_clients.items():
+                for _, client in self.connected_clients.items():
                     client["transport"] = self.socketio.server.transport(request.sid)
                 if current_user.role == "admin":
                     self.socketio.emit("clients", self.connected_clients, room=sid)
@@ -128,7 +132,7 @@ class wsServer(BasePlugin):
         @self.socketio.on("setProperty")
         def handleSetProperty(name, value):
             try:
-                self.logger.debug("Received setProperty: %s=%s", name,value )
+                self.logger.debug("Received setProperty: %s=%s", name, value)
                 setProperty(name, value, "WS")
             except Exception as ex:
                 self.logger.exception(ex, exc_info=True)
@@ -139,14 +143,14 @@ class wsServer(BasePlugin):
                 self.logger.debug("Received callMethod: %s", name)
                 result = callMethod(name)
                 sid = request.sid
-                data ={"name":name,"data":result}
+                data = {"name": name, "data": result}
                 self.socketio.emit("resultCallMethod", data, room=sid)
             except Exception as ex:
                 self.logger.exception(ex, exc_info=True)
 
     def changeProperty(self, obj, prop, value):
         try:
-            cache_render = None # cache.get(f"render_{obj}")
+            cache_render = None     # cache.get(f"render_{obj}")
             name = obj + "." + prop
             for sid, client in list(self.connected_clients.items()):
                 if name in client["subsProperties"]:
@@ -161,21 +165,16 @@ class wsServer(BasePlugin):
                         o = getObject(obj)
                         with self._app.app_context():
                             cache_render = o.render()
-                        #cache.set(f"render_{obj}",cache_render,timeout=5)
+                        # cache.set(f"render_{obj}",cache_render,timeout=5)
                     message = {"object": obj, "value": cache_render}
                     self.socketio.emit("changeObject", message, room=sid)
         except Exception as ex:
             self.logger.exception(ex, exc_info=True)
 
-    def say(self, message, level=0, image=None, destination=None):
+    def say(self, message, level=0, args=None):
         try:
             for sid, _ in list(self.connected_clients.items()):
-                data = {
-                        "message": message,
-                        "level": level,
-                        "image": image,
-                        "destination": destination,
-                }
+                data = {"message": message, "level": level, "args": args}
                 self.socketio.emit("say", data, room=sid)
         except Exception as ex:
             self.logger.exception(ex, exc_info=True)
